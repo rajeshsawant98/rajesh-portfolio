36ff08b95635b2b071daebe3078cafda9c38558d
Rajesh Sawant <rajeshsawant98@gmail.com>
2025-06-10 03:09:36 -0700

    Remove unused 'id' and 'percent' from SkillCard props for cleaner code

--- components/Home/Skills/SkillCard.tsx
+++ components/Home/Skills/SkillCard.tsx
@@ -11,7 +11,7 @@ type SkillCardProps = {
 };
 
 const SkillCard = ({ skill }: SkillCardProps) => {
-  const { id, title, image, percent } = skill;
+  const {  title, image } = skill;
   return <div className="p-6 hover:bg-blue-900 duration-200 transition-all cursor-pointer text-center rounded-lg bg-gray-900">
     <Image src={image} alt={title} height={80} width={80} className="object-cover mx-auto"/>
     <h1 className="text-white text-[18px] font-[600] mt-4">{title}</h1>
