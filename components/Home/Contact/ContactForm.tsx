303f8858c927b2d64b41202df5c71af2e356b8cb
Rajesh Sawant <rajeshsawant98@gmail.com>
2025-06-10 16:01:30 -0700

    Fix apostrophe rendering in ContactForm component

--- components/Home/Contact/ContactForm.tsx
+++ components/Home/Contact/ContactForm.tsx
@@ -4,7 +4,7 @@ const ContactForm = () => {
   return (
     <div className="bg-[#140c1c] rounded-lg p-4 sm:p-10 ">
       <h1 className="text-bg text-2xl md:text-3xl lg:text-[2.5rem] font-bold">
-        Let's Connect
+          Let&apos;s Connect
       </h1>
       <p className="text-gray-200 mt-3 lg:text-base text-xs md:text-sm">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
