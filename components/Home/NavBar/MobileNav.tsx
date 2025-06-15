990a7f158348fafb8cf503ffda4d7b796c3cdda1
Rajesh Sawant <rajeshsawant98@gmail.com>
2025-06-15 02:32:36 -0700

    Comment out unused Link import in MobileNav component

--- components/Home/NavBar/MobileNav.tsx
+++ components/Home/NavBar/MobileNav.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { navLinks } from "@/constant/constant";
-import Link from "next/link";
+//import Link from "next/link";
 import { CgClose } from "react-icons/cg";
 
 type Props = {
